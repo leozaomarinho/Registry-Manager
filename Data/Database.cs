2413f452f6c091ad38456f5063343842bf86bf65
leonardo <leowalkg6@Gmail.com>
2025-12-22 22:35:12 -0300

    iniciando projeto

---
+++ Data/Database.cs
@@ -0,0 +1,8 @@
+using StudentManagement.Models;
+using System.Collections.Generic;
+
+public Student Add(Student student)//Criacao
+public List<Student> GetAll()//Leitura de todos os registros
+public Student GetById(int id)//Leitura de um unico registro
+public bool Update(Student student)//Atualizacao
+public bool Delete(int id)//Remocao
---
+++ Validators/StudentValidator.cs
@@ -0,0 +1,8 @@
+using StudentManagement.Models;
+
+public static bool Validate(Student student, out List<string> errors)
+{
+    //regras de negocio
+    //validacao dos dados
+
+};
