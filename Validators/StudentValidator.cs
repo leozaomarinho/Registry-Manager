22b3427d82f2ba70caf332ccf881fca0d8a9083c
leonardo <leowalkg6@Gmail.com>
2025-12-22 22:40:46 -0300

    fix lint on methods

--- Validators/StudentValidator.cs
+++ Validators/StudentValidator.cs
@@ -1,6 +1,7 @@
 using StudentManagement.Models;
+using System.Collections.Generic;
 
-public static bool Validate(Student student, out List<string> errors)
+static bool Validate(Student student, out List<string> errors)
 {
     //regras de negocio
     //validacao dos dados
